819dacecd45d076d98a5e6d0ef897e819c8ab49d
mookofski <76699383+mookofski@users.noreply.github.com>
2021-03-17 00:38:46 +0900

    FUCK ANIMATION CONTROL

---
+++ Assets/Assets/Managers/MG/TL_Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TL_Manager : MonoBehaviour
+{
+
+    //*
+    //* PURPOSE
+    //* =INTERFACE TO EVERY TIMELINES
+    //*
+    //* REQ
+    //*
+    //* =TAKE ALL TIMELINES
+    //*
+    //*
+    //*
+    
+  public static  List<PC_Inst_Timeline> TimelineList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimelineList=new List<PC_Inst_Timeline>();
+        TimelineList.AddRange(FindObjectsOfType<PC_Inst_Timeline>());
+    }
+
+}
